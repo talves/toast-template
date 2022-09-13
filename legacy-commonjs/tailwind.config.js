1d90cbadcf24554520e7f7372d44a49fcc233a6a
talves <tony@3alves.com>
2022-09-13 12:22:48 -0700

    update: bump tailwind

--- legacy-commonjs/tailwind.config.js
+++ legacy-commonjs/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: ["./src/**/*.js", "./content/**/*.mdx"],
+  content: ["./src/**/*.js", "./content/**/*.mdx"],
   theme: {},
   variants: {},
   plugins: [
